d9bcd3aec885cd063c002b5fcaf6a73a44942b96
alx4ndre <mateo.mero@udla.edu.ec>
2025-10-17 13:38:13 -0500

    Cambios Realizados en Actividad 4

--- ACT-SEMANA-4-MERO.c
+++ ACT-SEMANA-4-MERO.c
@@ -1,25 +1,35 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main() 
 {
-    int Mero[5][5];  
+    int filas, columnas;
+
+    printf("Ingresar un numero de filas para la matriz MERO: ");
+    scanf("%d", &filas);
+    printf("Ingrese un numero de columnas para la matriz MERO: ");
+    scanf("%d", &columnas);
+
+    int Mero[filas][columnas];
+
     
-   
-    for(int i = 0; i < 5; i++){
-        for (int j = 0; j < 5; j++) {
-            Mero[i][j] = 0;
+    for(int i = 0; i < filas; i++){
+        for(int j = 0; j < columnas; j++){
+            if(i == j)
+                Mero[i][j] = 1;
+            else
+                Mero[i][j] = 0;
         }
     }
 
-    
-    printf("Mostrar matriz Mero 5x5 solo con ceros:\n");
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
-            printf("%3d ", Mero[i][j]); 
+    printf("\nMatriz Mero con diagonal en 1:\n");
+    for(int i = 0; i < filas; i++){
+        for(int j = 0; j < columnas; j++){
+            printf("%d ", Mero[i][j]);
         }
         printf("\n");
     }
 
     return 0;
 }
+
 
